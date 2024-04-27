73e558dd37d528dab2761781e5cc4cd09cde34e3
main <main@miyazakitakuyanoMacBook-Air.local>
2024-04-27 20:45:27 +0900

    AMIを変更

--- src-system/lib/src-system-stack.ts
+++ src-system/lib/src-system-stack.ts
@@ -97,7 +97,7 @@ export class SystemStack extends Stack {
     );
 
     // EC2(ASG)
-    const amiId = "ami-0addf8b394eea8913";
+    const amiId = "ami-0238382299c32c7a7";
     const autoscalingGroup = new AutoScalingGroup(this, "autoscalingGroup", {
       vpc: vpc,
       instanceType: InstanceType.of(InstanceClass.T2, InstanceSize.MICRO),
